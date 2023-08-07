4f30edab0b1ac45adc5f6fee9dfc87e06e44a8c4
Ubuntu <azureuser@testcicd.34kyqc4uimnudhx3sbx2nv4kbg.hx.internal.cloudapp.net>
2023-08-07 04:50:56 +0000

    auto install docker

--- install_docker.sh
+++ install_docker.sh
@@ -12,7 +12,7 @@ echo \
 
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo docker run hello-world
 
 sudo chmod 666 /var/run/docker.sock
