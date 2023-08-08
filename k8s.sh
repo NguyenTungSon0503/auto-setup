a91c339a58722ef3d0e7055a63e1a3c3eff859d3
Nguyễn Tùng Sơn <sonmeliodas0503@gmail.com>
2023-08-09 04:34:23 +0700

    update

--- k8s.sh
+++ k8s.sh
@@ -63,3 +63,5 @@ kubectl taint node $(hostname) node-role.kubernetes.io/master:NoSchedule-
 wget https://get.helm.sh/helm-v3.7.2-linux-amd64.tar.gz
 tar -xvf helm-v3.7.2-linux-amd64.tar.gz
 sudo mv linux-amd64/helm /usr/local/bin/
+
+#kubeadm token create --print-join-command
